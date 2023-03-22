b83e987be7c0c1a165e5869552815d3ff249165b
Samyak Piya <samyakpiya@gmail.com>
2023-03-21 21:23:56 -0400

    create basic file structure

---
+++ app/_layout.js
@@ -0,0 +1,28 @@
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+// When the app is initially loading, it's going to make the splash screen visible
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      // only show the homepage if the fonts have been loaded
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
+};
+
+export default Layout;
---
+++ hook/useFetch.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/search`,
+    params: {
+      query: "Python developer in Texas, USA",
+      page: "1",
+      num_pages: "1",
+    },
+    headers: {
+      "X-RapidAPI-Key": "75bb70ea9fmsha2f632d8fc7a922p174f24jsnc804fc60f502",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
+};
